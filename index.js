9949f7c2690f958d865ecf16d22977c9ce2d0c1a
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-03-31 01:39:01 +0600

    pushing to server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 // const DB_USER = 'flourishblottsmanager'
 // const DB_PASS = 'alohomora'
 // const database = 'flourishblotts'
-console.log({ process.env.DB_PASS })
+// console.log({ process.env.DB_PASS })
 
 app.get('/', (req, res) => {
     res.send('Hello Wizards and Witches!')
